6c90a0a3d0a6da21ec8424786025511df6a23c4b
Antonio Semaan <sem3ane_9889@hotmail.com>
2024-07-15 01:22:39 +0300

    initial commit. added portfolio project. added main components

---
+++ portfolio/src/app/components/site-content/site-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-content',
+  templateUrl: './site-content.component.html',
+  styleUrl: './site-content.component.css'
+})
+export class SiteContentComponent {
+
+}
